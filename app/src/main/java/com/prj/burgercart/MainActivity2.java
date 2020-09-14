bc40b706605e925f11d393555ccffc3799641dde
youssefgamal111 <58340249+youssefgamal111@users.noreply.github.com>
2020-09-14 17:57:13 +0200

    young

--- app/src/main/java/com/prj/burgercart/MainActivity2.java
+++ app/src/main/java/com/prj/burgercart/MainActivity2.java
@@ -1,10 +1,16 @@
 package com.prj.burgercart;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.myapplication.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -12,5 +18,28 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        EditText time=(EditText) findViewById(R.id.time);
+        Calendar c = Calendar.getInstance();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        final String formattedDate = df.format(c.getTime());
+        time.setText(formattedDate);
+        time.setEnabled(false);
+        final EditText status=(EditText) findViewById(R.id.status);
+        status.setText("notcompleted");
+        status.setEnabled(false);
+        final OrdersDB neworder =new OrdersDB(this);
+        final EditText id=(EditText) findViewById(R.id.id);
+        id.setText(neworder.getorderid());
+        id.setEnabled(false);
+        Button b=(Button)findViewById(R.id.button);
+        final EditText describtion=(EditText) findViewById(R.id.description);
+        final EditText details=(EditText) findViewById(R.id.details);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                neworder.createneworder(neworder.getorderid(),formattedDate,describtion.getText().toString(),details.getText().toString());
+
+            }
+        });
     }
 }
--- app/src/main/java/com/prj/burgercart/OrdersDB.java
+++ app/src/main/java/com/prj/burgercart/OrdersDB.java
@@ -1,39 +1,45 @@
+
 package com.prj.burgercart;
-
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteOpenHelper;
-
 public class OrdersDB extends SQLiteOpenHelper {
-
     private static String databaseName = "ordersDatabase";
-    SQLiteDatabase ordersDatabase;
-
+    SQLiteDatabase ordersdatabase;
     public OrdersDB(Context context) {
-        super(context,databaseName,null,1);
+        super(context,databaseName,null,1);}
+    public void createneworder(int id, String time, String description, String details) {
+        ContentValues row = new ContentValues();
+        row.put("id", id);
+        row.put("time", time);
+        row.put("description", description);
+        row.put("details", details);
+        row.put("status", "noncompleted");
+        ordersdatabase = getWritableDatabase();
+        ordersdatabase.insert("orders", null, row);
+        ordersdatabase.close();
+    }
+
+    public int getorderid() {
+        ordersdatabase = getWritableDatabase();
+        Cursor cursor = ordersdatabase.rawQuery("SELECT  * FROM ordersdatabase", null);
+        int count = cursor.getCount();
+        cursor.close();
+        return count;
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table orders(id integer primary key,"+
                 "name text not null, description text)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists orders");
         onCreate(db);
-    }
-
-    public Cursor fetchAllOrders()
-    {
-        ordersDatabase = getReadableDatabase();
-        String [] rowDetails = {"name","description","id"};
-        Cursor cursor = ordersDatabase.query("orders",rowDetails,null,null,null,null,null);
-        if(cursor != null)
-            cursor.moveToFirst();
-        ordersDatabase.close();
-        return cursor;
-    }
-}
+    }}
