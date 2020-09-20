812b6355a90e7e550a19bd8fc86244bc41e9cdf4
MIDOKIM <47168819+MIDOKIM@users.noreply.github.com>
2020-09-20 05:13:39 +0200

    Update MainActivity7.java

--- app/src/main/java/com/prj/burgercart/MainActivity7.java
+++ app/src/main/java/com/prj/burgercart/MainActivity7.java
@@ -21,11 +21,15 @@ public class MainActivity7 extends AppCompatActivity {
 OrdersDB h=new OrdersDB(this);
 
 Cursor cursor=h.showhistoryorders(data.username);
+        String []arr = new String[cursor.getCount()];
 
-String []arr ;
+
+        ArrayAdapter<String> listadapter=new ArrayAdapter<>(this, R.layout.activity_main7,arr);
+
+        list.setAdapter(listadapter);
+
         if (cursor != null && cursor.getCount()>0){
             cursor.moveToFirst();
-           arr = new String[cursor.getCount()];
             do{
                 for(int i = 0; i < cursor.getCount(); i++){
                    arr[i]=(cursor.getString(i));
@@ -33,7 +37,5 @@ String []arr ;
             }while(cursor.moveToNext());
         }
         else{ arr = new String[1];arr[0]="NO PAST ORDERS";}
-        ArrayAdapter<String> listadapter=new ArrayAdapter<>(this, R.layout.activity_main7,arr);
-        list.setAdapter(listadapter);
     }
 }
